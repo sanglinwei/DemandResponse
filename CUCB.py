3a08642683ae7d047743d8d73e6ada54e44c122f
sanglinwei <sanglinwei21@163.com>
2019-10-21 22:05:05 +0800

    one situation verification
    function verification

--- CUCB.py
+++ CUCB.py
@@ -1,20 +1,18 @@
 # coding=UTF-8
 __author__ = 'sanglinwei'
 '''
-this code is following b
+this code 
 '''
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 
-def run_cucb(_knowledge, _user_prob, _event_num, _target):
-    k = _knowledge[['index', 'sit1', 'UCB1', 'time1', 'signal']]
+def run_ucb(knowledge, user_prob, _event_num, _target):
+    k = knowledge.copy()
     # plot the graph
     arm1 = list([])
     for j in range(_event_num):
-        # CUCB
         # calculate UCB
         ucb1 = k['sit1'] + np.sqrt(alpha * np.log(j+1) / (2 * k['time1']))
         k.loc[:, 'UCB1'] = ucb1
@@ -36,106 +34,128 @@ def run_cucb(_knowledge, _user_prob, _event_num, _target):
         for i in range(user_num):
             if k.loc[i, 'signal']:
                 # feedback
-                _feedback = np.random.binomial(1, _user_prob['sit1'][i])
+                _feedback = np.random.binomial(1, user_prob['sit1'][i])
                 # update
                 k.loc[i, 'sit1'] = (k.loc[i, 'sit1'] * k.loc[i, 'time1'] + _feedback) / (k.loc[i, 'time1'] + 1)
                 k.loc[i, 'time1'] = k.loc[i, 'time1'] + 1
         arm1.append(k.loc[1, 'sit1'])
-    # update the knowledge
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.scatter(range(event_num), arm1)
-    plt.show()
-    _knowledge.loc[:, 'sit1'] = k['sit1']
-    _knowledge.loc[:, 'time1'] = k['time1']
-    return _knowledge
+
+    return k
 
 
-def run_concucb(_kn, _user, _event, ):
+def run_ucb_in_contextual(knowledge, user_prob, _events, _target):
+    k = knowledge.copy()
+
+    events_str = list(map(str, _events))
+    num = 1
+    for event_id in events_str:
+        # different situation
+        sit_choose = 'sit' + event_id
+        # index
+        ucb = k['sit1'] + np.sqrt(alpha * np.log(num) / (2 * k['time1']))
+        k.loc[:, 'UCB1'] = ucb
+        k.sort_values(by=['UCB1'], ascending=False, inplace=True)
+        m = k['sit1'].tolist()
+        accumulate = list([])
+
+        for i in range(user_num):
+            accumulate.append(sum(m[0:i + 1]))
+        signal = list([x < _target for x in accumulate])
+        k.loc[:, 'signal'] = signal
+        k.sort_values(by=['index'], ascending=True, inplace=True)
+
+        for i in range(user_num):
+            if k.loc[i, 'signal']:
+                # feedback
+                feedback = np.random.binomial(1, user_prob[sit_choose][i])
+                # update
+                k.loc[i, 'sit1'] = (k.loc[i, 'sit1'] * k.loc[i, 'time1'] + feedback) / (k.loc[i, 'time1'] + 1)
+                k.loc[i, 'time1'] = k.loc[i, 'time1'] + 1
+        num = num + 1
+
+    return k
 
 
-    return 0
+def run_contextual_ucb(knowledge, user_prob, _events, _target):
+    k = knowledge.copy()
+
+    events_str = list(map(str, _events))
+    num_sit = np.zeros(3)
+
+    for event_id in events_str:
+        # different situation
+        sit_choose = 'sit' + event_id
+        time_choose = 'time' + event_id
+        ucb_choose = 'UCB' + event_id
+        # index
+        ucb = k[sit_choose] + np.sqrt(alpha * np.log(num_sit[int(event_id) - 1] + 1) / (2 * k[time_choose]))
+        k.loc[:, ucb_choose] = ucb
+        k.sort_values(by=[ucb_choose], ascending=False, inplace=True)
+        m = k[sit_choose].tolist()
+        accumulate = list([])
+
+        for i in range(user_num):
+            accumulate.append(sum(m[0:i + 1]))
+        signal = list([x < _target for x in accumulate])
+        k.loc[:, 'signal'] = signal
+        k.sort_values(by=['index'], ascending=True, inplace=True)
+
+        for i in range(user_num):
+            if k.loc[i, 'signal']:
+                # feedback
+                feedback = np.random.binomial(1, user_prob[sit_choose][i])
+                # update
+                k.loc[i, sit_choose] = (k.loc[i, sit_choose] * k.loc[i, time_choose] + feedback) / \
+                                       (k.loc[i, time_choose] + 1)
+                k.loc[i, time_choose] = k.loc[i, time_choose] + 1
+        num_sit[int(event_id) - 1] = num_sit[int(event_id) - 1] + 1
+    return k
 
 
+# scale the probability
 def prob_generate(num):
     prob = 10*np.random.randn(num)+90
     prob[prob > 90] = np.random.randn(sum(prob > 90))*2+90
     return prob/100
 
 
-user_num = 5
-event_num = 200
-alpha = 2  # UCB parameter
+if __name__ == '__main__':
+    # fundamental parameters
+    user_num = 10  # the number of participated customers
+    event_num = 200  # the number of demand response event
+    alpha = 2  # UCB parameter
 
-# initial configuration
-# user's configuration
-User_PROB = pd.DataFrame(columns=['index', "sit1", "sit2", "sit3"])
-User_PROB["sit1"] = prob_generate(user_num)
-User_PROB["sit2"] = prob_generate(user_num)
-User_PROB["sit3"] = prob_generate(user_num)
-User_PROB['index'] = range(user_num)
+    # initial configuration
+    # user's configuration
+    User_PROB = pd.DataFrame(columns=['index', "sit1", "sit2", "sit3"])
+    User_PROB["sit1"] = prob_generate(user_num)
+    User_PROB["sit2"] = prob_generate(user_num)
+    User_PROB["sit3"] = prob_generate(user_num)
+    User_PROB['index'] = range(user_num)
 
-# power system command configuration in
-target = 3  # fixed target
+    # power system command configuration in
+    target = 3  # fixed target which can be time-varying
 
-# demand aggregator configuration
-KNOWLEDGE_INIT = pd.DataFrame(columns=['index', "sit1", 'UCB1', "sit2", 'UCB2', "sit3", 'UCB3',
-                                       "power", "time1", 'time2', 'time3', 'signal'])
-KNOWLEDGE_INIT['sit1'] = np.zeros(user_num)+0.2
-KNOWLEDGE_INIT['sit2'] = np.zeros(user_num)+0.2
-KNOWLEDGE_INIT['sit3'] = np.zeros(user_num)+0.2
-KNOWLEDGE_INIT['power'] = 200
-KNOWLEDGE_INIT['time1'] = 4
-KNOWLEDGE_INIT['time2'] = 4
-KNOWLEDGE_INIT['time3'] = 4
-KNOWLEDGE_INIT['index'] = range(user_num)
-KNOWLEDGE_INIT['signal'] = 0
-knowledge = KNOWLEDGE_INIT
+    # demand aggregator configuration
+    KNOWLEDGE_INIT = pd.DataFrame(columns=['index', "sit1", 'UCB1', "sit2", 'UCB2', "sit3", 'UCB3',
+                                           "power", "time1", 'time2', 'time3', 'signal'])
+    KNOWLEDGE_INIT['sit1'] = np.zeros(user_num)+0.2
+    KNOWLEDGE_INIT['sit2'] = np.zeros(user_num)+0.2
+    KNOWLEDGE_INIT['sit3'] = np.zeros(user_num)+0.2
+    KNOWLEDGE_INIT['power'] = 200
+    KNOWLEDGE_INIT['time1'] = 1
+    KNOWLEDGE_INIT['time2'] = 1
+    KNOWLEDGE_INIT['time3'] = 1
+    KNOWLEDGE_INIT['index'] = range(user_num)
+    KNOWLEDGE_INIT['signal'] = 0
+    k1 = KNOWLEDGE_INIT.copy()
+    k2 = KNOWLEDGE_INIT.copy()
+    k3 = KNOWLEDGE_INIT.copy()
 
-# knowledge = run_cucb(KNOWLEDGE_INIT, User_PROB, event_num, target)
-k = knowledge
-
-# generate random sequence
-events = list(np.random.permutation(range(event_num)) % 3 + 1)
-
-# events = [1, 3, 2, 3]
-events_str = list(map(str, events))
-num_sit = np.zeros(3)
-
-for event_id in events_str:
-    # different situation
-    sit_choose = 'sit' + event_id
-    time_choose = 'time' + event_id
-    ucb_choose = 'UCB' + event_id
-    # index
-    ucb = k[sit_choose] + np.sqrt(alpha*np.log(num_sit[int(event_id)-1]+1)/(2*k[time_choose]))
-    k.loc[:, ucb_choose] = ucb
-    k.sort_values(by=[ucb_choose], ascending=False, inplace=True)
-    m = k[sit_choose].tolist()
-    accumulate = list([])
-    for i in range(user_num):
-        accumulate.append(sum(m[0:i+1]))
-    signal = list([x < target for x in accumulate])
-    k.sort_values(by=['index'], ascending=True, inplace=True)
-    k.loc[:, 'signal'] = signal
-    print(event_id)
-
-    for i in range(user_num):
-        if k.loc[i, 'signal']:
-            # feedback
-            feedback = np.random.binomial(1, User_PROB[sit_choose][i])
-            # update
-            k.loc[i, sit_choose] = (k.loc[i, sit_choose] * k.loc[i, time_choose] + feedback) / \
-                                   (k.loc[i, time_choose] + 1)
-            k.loc[i, time_choose] = k.loc[i, time_choose] + 1
-    num_sit[int(event_id) - 1] = num_sit[int(event_id) - 1]+1
-    # knowledge update
-    knowledge.loc[:, sit_choose] = k[sit_choose]
-    knowledge.loc[:, time_choose] = k[time_choose]
-
-
-
-
-
+    # generate random sequence
+    events = list(np.random.permutation(range(event_num)) % 3 + 1)
+    knowledge_common = run_ucb(k1, User_PROB, event_num, target)
+    knowledge_common_in_contextual = run_ucb_in_contextual(k2, User_PROB, events, target)
+    knowledge_contextual = run_contextual_ucb(k3, User_PROB, events, target)
 
 
---
+++ user.py
@@ -0,0 +1,8 @@
+import numpy
+
+
+class User:
+    Pro_sit1 = 0.0
+    Pro_sit2 = 0.0
+    Pro_sit3 = 0.0
+    chosen_times = 0
